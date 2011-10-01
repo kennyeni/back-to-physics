460446ee5d9875b5c2194bb10a8b3ec703d88741
brill.kennyzeniant@gmail.com <brill.kennyzeniant@gmail.com@a5f5ca8f-89d9-a928-7468-a51dd03b7032>
2011-10-01 22:05:44 +0000

    Loading screen lista

--- trunk/Back to Physics/src/menu/PantallaJuego.java
+++ trunk/Back to Physics/src/menu/PantallaJuego.java
@@ -5,10 +5,13 @@ import graficas.Coordenadas;
 import graficas.Juego;
 import graficas.Pantalla;
 import android.app.Activity;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnLongClickListener;
 import android.view.View.OnTouchListener;
 
 
@@ -36,6 +39,14 @@ public class PantallaJuego  extends Activity implements Runnable, OnTouchListene
    	 player=MediaPlayer.create(this,mx.itesm.btp.R.raw.pkmn);
    	 player.start();
     }
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if(keyCode == KeyEvent.KEYCODE_BACK){
+			startActivity(new Intent(PantallaJuego.this, Principal.class));
+		}
+		return true;
+	}
 
 
 	protected void onResume() {
@@ -77,10 +88,8 @@ public class PantallaJuego  extends Activity implements Runnable, OnTouchListene
 	
 	
 	
-	
-	
-	
 	public boolean onTouch(View v, MotionEvent event) {
+		
 		try {
 			juego.fondo.mueveX(10);
 			juego.fondo.mueveY(10);
--- trunk/Back to Physics/src/menu/Principal.java
+++ trunk/Back to Physics/src/menu/Principal.java
@@ -1,11 +1,15 @@
 package menu;
 
+import graficas.Juego;
 import mx.itesm.btp.*;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,7 +20,7 @@ import android.view.View.OnTouchListener;
 
 
 public class Principal extends Activity implements OnTouchListener{
-    
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
          super.onCreate(savedInstanceState);
@@ -48,18 +52,61 @@ public class Principal extends Activity implements OnTouchListener{
     }
 
 	@Override
-	public boolean onTouch(View v, MotionEvent event) {
-		Intent intencion = null;
-		switch (v.getId()) {
-		case R.id.btnJuegoNuevo:intencion = new Intent(Principal.this,PantallaJuego.class); break;
-		case R.id.btnOpciones:intencion = new Intent(Principal.this,Opciones.class); break;
-		case R.id.btnAcercaDe:intencion = new Intent(Principal.this,acercaDe.class); break;
-		case R.id.btnContinuar:intencion = new Intent(Principal.this,Principal.class); break; //Implementar
-		case R.id.btnScores:intencion = new Intent(Principal.this, Principal.class); break;
+	public boolean onTouch(View v, MotionEvent event) {		
+		if(v.getId()==R.id.btnJuegoNuevo){
+			ejecutaJuego();
 		}
-		
-		startActivity(intencion);
+		else{
+			if(v.getId()==R.id.btnOpciones){
+				Intent intencion = new Intent(Principal.this,Opciones.class); 
+				startActivity(intencion); 
+				return false;
+			}
+			else{
+				if(v.getId()==R.id.btnAcercaDe){
+					Intent intencion = new Intent(Principal.this,acercaDe.class); 
+					startActivity(intencion); 
+					return false;
+				}
+			}
+		}
 		return false;
+	}
+	
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if(keyCode == KeyEvent.KEYCODE_BACK){
+			super.onDestroy();
+		}
+		return true;
+	}
+
+	private void ejecutaJuego() {
+		
+		Intent intencion = new Intent(Principal.this,Loading.class);
+		startActivity(intencion);
+		
+		AsyncTask<Context, Integer, Intent> sync = new AsyncTask<Context, Integer, Intent>() {
+			@Override
+			protected Intent doInBackground(Context... params) {
+				
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				Intent intencion = new Intent(Principal.this,PantallaJuego.class);
+				return intencion;
+			}
+			
+			@Override
+			protected void onPostExecute(Intent result) {
+				super.onPostExecute(result);
+				startActivity(result);
+			}
+		}; 
+		
+		sync.execute(this);
 	}
 
     
