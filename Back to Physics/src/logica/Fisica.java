6e5f6131d64f05544c70778ec39d7a50a280e30d
brill.kennyzeniant@gmail.com <brill.kennyzeniant@gmail.com@a5f5ca8f-89d9-a928-7468-a51dd03b7032>
2011-11-04 05:55:25 +0000

    Modulo de fisica listo, la clase recibe parametros dados y regres una LL de puntos secuenciales a graficar en cada vista, lo demas es juego de niños :), mañana lo hago jaja, sueño :S

---
+++ Back to Physics/src/logica/Fisica.java
@@ -0,0 +1,63 @@
+package logica;
+
+import graficas.Coordenadas;
+
+import java.util.LinkedList;
+
+/**
+ * Clase estatica que regresa una matriz de puntos a graficar
+ * @author Kenny
+ *
+ */
+
+public class Fisica {
+	
+	/**
+	 * Clase que regresa una LL de puntos a graficar en la parte aerea
+	 * @param velocidad
+	 * @param theta
+	 * @param phi
+	 * @param gravedad
+	 * @return puntos
+	 */
+	public static LinkedList<Coordenadas> puntosAereos(double v, double theta, double phi, double g){
+		LinkedList<Coordenadas> puntos = new LinkedList<Coordenadas>();
+		double radian = Math.PI/180;
+		double vx = v*Math.cos(phi*radian);
+		double vy = v*Math.sin(phi*radian);
+		double t = 2*vy/g;
+		for(double a = 0; a <= t; a+=.001){
+			double hyp = vx*a;
+			float y = (float) a;
+			float x = (float) (hyp*Math.sin(theta*radian));
+			puntos.add(new Coordenadas(x, y));
+		}
+		
+		return puntos;
+	}
+	
+	/**
+	 * Clase que regresa una lista de LL de puntos a graficar en la parte lateral
+	 * @param velocidad
+	 * @param theta
+	 * @param phi
+	 * @param gravedad
+	 * @return puntos
+	 */
+
+	public static LinkedList<Coordenadas> puntosLaterales(double v, double theta, double phi, double g){
+		LinkedList<Coordenadas> puntos = new LinkedList<Coordenadas>();
+		double radian = Math.PI/180;
+		double vx = v*Math.cos(phi*radian);
+		double vy = v*Math.sin(phi*radian);
+		double t = 2*vy/g;
+		for(double a = 0; a <= t; a+=.001){	
+			float x = (float) a;
+			float y = (float) (vy*a-.5*g*a*a);
+			puntos.add(new Coordenadas(x, y));
+		}
+		return puntos;
+	}
+	
+
+}
