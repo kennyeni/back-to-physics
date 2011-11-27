f801f643adc4119b3bdd972702982e77cc3f2ffa
vefv.eunice@gmail.com <vefv.eunice@gmail.com@a5f5ca8f-89d9-a928-7468-a51dd03b7032>
2011-11-27 23:31:24 +0000

    Sabías que con datos. ;)

--- trunk/Back to Physics/src/menu/Loading.java
+++ trunk/Back to Physics/src/menu/Loading.java
@@ -4,6 +4,7 @@ import mx.itesm.btp.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /**
  * Clase que manda llamar  y crea al layout que muestra la pantalla se "sabias qué" mientras se carga el juego 
@@ -17,7 +18,12 @@ public class Loading extends Activity {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.loading);
+		TextView texto = (TextView) findViewById(R.id.textoSabias);
 		ProgressBar barra = (ProgressBar) findViewById(R.id.progressBar1);
+		
+		int res = (int) Math.round(Math.random()*8);
+		String[] texto2 = getResources().getStringArray(R.array.txtSabiasque);
+		texto.setText(texto2[res]);
 		
 		
 	}
